552ba169e9ffd21bb476eaa9fdbd46ad9e392aa6
louise gentillet <louise.gentillet@gmail.com>
2017-12-28 16:28:48 +0100

    Avec fréquence de paiement (fonction en cours)

---
+++ include/frequenceMontant.php
@@ -0,0 +1,53 @@
+<?php
+
+include("/Head.php");
+include("/Menu.php");
+$bdd=new PDO('mysql:host=localhost;dbname=tempsdanse', 'root', '');
+  include("/ConnexionBDD.php");
+  $NumAdh=4
+	$Req = $bdd->query("SELECT `Montant`, `FrequencePaiement` FROM adherent WHERE `NumAdh`=$NumAdh");
+	$MontantREQ=$Req->fetch();
+	$Montant=$MontantREQ["Montant"];
+  $Frequence=$MontantREQ["FrequencePaiement"];
+  echo "montant : $Montant, fréquence : $Frequence";
+
+  switch ($Frequence)
+  {
+  case 'Mensuel':
+    $Montant/=10;
+    break;
+  case 'Semestriel':
+    $Montant/=2;
+    break
+  case 'Trimestriel':
+     $Montant/=3;
+     break;
+  case 'Bimestriel':
+    $Montant/=5;
+    break;
+   default:
+    $Montant=$Montant;
+   break;
+  }
+  // $ReqReduc = $bdd->query("SELECT Reduction FROM `adherent` WHERE `NumAdh`=$NumAdh");
+  // $Reducfetch=$ReqReduc->fetch();
+  // $Reduc=$Reducfetch['Reduction'];
+  // if($Reduc!=NULL)
+  // {
+  //   switch ($Reduc)
+  //   {
+  //     case 'F2':
+  //     $t-=$t*0.2;
+  //     break;
+  //     case 'F3':
+  //     $t-=$t*0.3;
+  //     break;
+  //
+  //     default:
+  //     $t-=$t*0.1;
+  //     break;
+  //   }
+  // }
+  // echo "<p>Montant : ".$t."</p>";
+  // $bdd->query("UPDATE `adherent` SET`Montant`=$t WHERE NumAdh=$NumAdh");
+  ?>
