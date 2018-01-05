4cf46dc9dd8ed4ce90c5221d9a4158e58009529d
louise gentillet <louise.gentillet@gmail.com>
2018-01-05 11:06:05 +0100

    Suppression alerte ok !
    Penser plus aux session et à l'auto-formulaire

--- SupprimerAlerte.php
+++ SupprimerAlerte.php
@@ -1,44 +1,45 @@
 <?php
+session_start();
 include('include/Head.php');
 include('include/Menu.php');
 $bdd=new PDO('mysql:host=localhost;dbname=tempsdanse', 'root', '');
-$Id=$_GET['Id'];
-$Action=$_GET['A'];
-echo $Id $Action ?>
-<div>Vous êtes ici : <a href='index.php'>Accueil</a> - <a href='GestionAdh.php'>Gestion Adhérent</a> - Mise à Jour 3</div>
+?>
+
+<div>Vous êtes ici : <a href='index.php'>Accueil</a> - Supprimer Alerte</div>
 <section>
 	<h2>Supprimer Alerte</h2>
-	<h3><?php echo $Alerte;?></h3>
-  <p>Supprimer cette alerte ? </p>
-  <form action="SupprimerAlerte.php" method="post">
-    <input type="checkbox" name="OUI" value="OUI">OUI
-    <input type="checkbox" name="NON" value="NON">NON
-    <input type="submit">
-  </form>
-	<?php
-  if($A='compta')
-  {
-    if (isset($_POST['OUI'])) {
-      $EffAdh=$bdd->query("DELETE FROM alertecompta WHERE `IdAlerte`=$Id");
-      $EffAdh->closeCursor();
-      echo "<p>Alerte supprimée</p>";
-    }
-    else {
-      echo "<p>Alerte non supprimée</p>";
-    }
-  }
 
-		if($A='adh')
-		{
-      if (isset($_POST['OUI'])) {
-        $EffAdh=$bdd->query("DELETE FROM alerteadh WHERE `IdAlerte`=$Id");
-  			$EffAdh->closeCursor();
-  			echo "<p>Alerte supprimée</p>";
-      }
-      else {
-        echo "<p>Alerte non supprimée</p>";
-      }
+	<?php
+	if (isset($_POST['OUI'])) {
+		echo $_SESSION['Action'];
+
+		if($_SESSION['Action']=='compta')		{
+			$id=$_SESSION['Id'];
+			$EffAdh=$bdd->query("DELETE FROM `alertecompta` WHERE `IdAlerte`=$id");
+			$EffAdh->closeCursor();
+			echo "<p>Alerte supprimée</p>";
 		}
+
+		if($_SESSION['Action']=='adh')		{
+			$id=$_SESSION['Id'];
+			$EffAdh=$bdd->query("DELETE FROM alerteadh WHERE `IdAlerte`=$id");
+			$EffAdh->closeCursor();
+			echo "<p>Alerte supprimée</p>";
+		}
+		session_destroy();
+		echo "<a href='index.php'>Retour à l'Accueil</a>";
+	}
+
+	if(isset($_GET['A'])) {
+		$_SESSION['Id']=$_GET['Id'];
+		$_SESSION['Action']=$_GET['A'];?>
+		<p>Supprimer cette alerte ? </p>
+		<form action="SupprimerAlerte.php" method="post">
+			<input type="checkbox" name="OUI" value="OUI">OUI<br>
+			<input type="checkbox" name="NON" value="NON">NON<br><br>
+			<input type="submit">
+		</form><?php
+	}
 	?>
 
 </section>
--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $bdd=new PDO('mysql:host=localhost;dbname=tempsdanse', 'root', '');
 									<td><?php echo $Adh['Nom']." ".$Adh['Prenom']; ?></td>
 									<td><?php echo $donnees['Date']; ?></td>
 									<td><?php echo $donnees['Montant']; ?></td>
-									<td><a href="SupprimerAlerte.php?Id=<?php echo $donnees['IdAlerte'];?>,A='adh'">Supprimer</a><a href="??????????">Ajouter ligne</a></td>
+									<td><a href="SupprimerAlerte.php?Id=<?php echo $donnees['IdAlerte'];?>&A=adh">Supprimer</a><a href="??????????">Ajouter ligne</a></td>
 								</tr>
 								<?php
 								$AdhReq->closeCursor();
@@ -74,7 +74,7 @@ $bdd=new PDO('mysql:host=localhost;dbname=tempsdanse', 'root', '');
 									<td><?php echo $Adh['Motif']; ?></td>
 									<td><?php echo $donnees['Date']; ?></td>
 									<td><?php echo $donnees['Montant']; ?></td>
-									<td><a href="SupprimerAlerte.php?<?php echo "Id=$donnees['IdAlerte'],A=compta";?>">Supprimer</a><a href="??????????">Ajouter ligne</a></td>
+									<td><a href="SupprimerAlerte.php?Id=<?php echo $donnees['IdAlerte'];?>&A=compta">Supprimer</a><a href="??????????">Ajouter ligne</a></td>
 								</tr>
 								<?php
 								$AdhReq->closeCursor();
