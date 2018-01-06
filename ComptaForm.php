5530af5d541642784a49120cda35071925147931
louise gentillet <louise.gentillet@gmail.com>
2018-01-06 17:52:13 +0100

    FUUUUUCK

--- ComptaForm.php
+++ ComptaForm.php
@@ -16,7 +16,7 @@ if($Value){
 		$Ligne=$AdhReq->fetch();
 		$Motif=$Ligne['Motif'];
 		$Frequence=$Ligne['Frequence'];
-		}
+	}
 	if($Adh){
 		$AdhReq=$bdd->query("SELECT * FROM adherent WHERE NumAdh=$idLigne");
 		$Ligne=$AdhReq->fetch();
@@ -37,53 +37,78 @@ if($Value){
 
 	<form method="post" action="ComptaBDD.php">
 
-					<fieldset class="Info"><legend>Nouvelle ligne</legend>
-						<table class="Form">
-							<tr class="Form"><td class="Form"><label for="Motif">Motif : </label></td><td class="Form"><input type="text"
-								<?php
-								if ($Value) {
-										echo "value='$Motif'";
-								}
-								?> name="Motif" id="Motif" required />	</td></tr>
-							<tr class="Form"><td class="Form"><label for="Date">Date : </label></td><td class="Form"><input type="text"
-								<?php
-								if ($Value) {
-										echo "value=$Date";
-								}
-								?> name="Date" id="Date" required placeholder="aaaa-mm-jj"/></td></tr>
+		<fieldset class="Info"><legend>Nouvelle ligne</legend>
+			<table class="Form">
+
+<!--Montif-->
+				<tr class="Form"><td class="Form"><label for="Motif">Motif : </label></td><td class="Form"><input type="text"
+					<?php
+					if ($Value) {
+						echo "value='$Motif'";
+					}
+					?> name="Motif" id="Motif" required />	</td></tr>
+					<tr class="Form"><td class="Form"><label for="Date">Date : </label></td>
+						<td class="Form"><input type="text"
+							<?php
+							if ($Value) {
+								echo "value=$Date";
+							}
+							?> name="Date" id="Date" required placeholder="aaaa-mm-jj"/></td></tr>
+
+<!--Montant-->
 							<tr class="Form"><td class="Form"><label for="Montant"> Montant :  </label></td><td class="Form"><input required type="text"
 								<?php
 								if ($Value) {
-										echo "value=$Montant";
+									echo "value=$Montant";
 								}
 								?> name="Montant" id="Montant" /></td></tr>
-						</table>
-						<p><strong>Fréquence : </strong></p>
-							<select name="Frequence" <?php
+							</table>
 
-							echo ">";
-								$reponse = $bdd->query('SELECT * FROM frequence');
-								while ($donnees = $reponse->fetch())
-								{
-									$freq=$donnees['NomFrequence'];
-									if ($Value) {
-											if($freq)
-									}
+<!--Fréquence-->
+							<p><strong>Fréquence : </strong></p>
+							<select name="Frequence" > <?php
+							$reponse = $bdd->query('SELECT * FROM frequence');
+							if ($Value) {
+								echo "<option required value=\"$Frequence\">$Frequence</option>";
+							}
+							while ($donnees = $reponse->fetch())
+							{
+								$freq=$donnees['NomFrequence'];
+								if ($Value && $freq!=$Frequence) {
 									echo "<option required value=\"$freq\">$freq</option>";
 								}
-								$reponse->closeCursor();
-								?>
-								</select>
+								else {
+									echo "<option required value=\"$freq\">$freq</option>";
+								}
+							}
+							$reponse->closeCursor();
+							?>
+						</select>
 
-							<p><strong>Type : </strong></p>
-							<select name="Type">
-								<?php
-								echo ">";
-								$reponse = $bdd->query('SELECT * FROM typecompta');
-								while ($donnees = $reponse->fetch())
-								{
-									$Id=$donnees['Type'];
+<!--Type-->
+
+						<p><strong>Type : </strong></p>
+						<select name="Type">
+							<?php
+							$reponse = $bdd->query('SELECT * FROM typecompta');
+							if ($Value) {
+								if ($Adh) {
+									echo "<option value=\"Recette\"> Recette</option>";
+									echo "<option value=\"Depense\"> Depense</option>";
+								}
+							}
+							while ($donnees = $reponse->fetch())
+							{
+								$Id=$donnees['Type'];
+								if ($Value) {
+									if ($compta) {
+										echo "<option value=\"$Id\"> $Id</option>";
+									}
+								}
+								else {
 									echo "<option value=\"$Id\"> $Id</option>";
+								}
+
 								}
 								$reponse->closeCursor();
 								?>
@@ -92,23 +117,32 @@ if($Value){
 						</fieldset>
 					</td>
 
+<!--Facultatif-->
+
 					<td class="invisible">
 						<fieldset class="Cours"><legend>Facultatif</legend>
+
+<!--Mode de paiement-->
+
 							<p><strong>Moyen de Paiement : </strong></p>
 							<select name="MoyenPaiement">
-								<?php
+              <?php
 								$reponse = $bdd->query('SELECT * FROM modepaiement');
-								while ($donnees = $reponse->fetch())
-								{
+								if ($Value) {
+									$NomReq = $bdd->query("SELECT * FROM modepaiement WHERE IdPaiement='$ModePaiement'");
+									$Nom=$NomReq->fetch();
+									?><option required value="<?php echo $ModePaiement;?>"><?php echo $Nom['NomPaiement'];?></option><?								}
+								while ($donnees = $reponse->fetch()){
 									$Id=$donnees['IdPaiement'];
 									$Nom=$donnees['NomPaiement'];
-									if ($Id==$ModePaiement) {
-										echo "<option autofocus value=\"$Id\">$Nom</option>";
+
+									if ($Value && $ModePaiement!=$Id) { ?><option required value="<?php echo $Id;?>"><?php echo $Nom;?></option><?php
 									}
-									echo "<option value=\"$Id\">$Nom</option>";
+									else { ?>
+										<option required value="<?php echo $Id;?>"><?php echo $Nom; ?></option><?php
+									}
 								}
-								$reponse->closeCursor();
-								?>
+								$reponse->closeCursor(); ?>
 							</select>
 
 							<p><strong>Catégorie : </strong></p>
@@ -116,10 +150,9 @@ if($Value){
 								<?php
 								if ($Value) {
 									if ($Adh) {
-										echo "value=\"Adherent\"";
+										?><option value='<?php echo $Id;?>'><?php echo $Id;?></option><?php
 									}
 								}
-								echo ">";
 								$reponse = $bdd->query('SELECT * FROM categorie ORDER BY Nom');
 								while ($donnees = $reponse->fetch())
 								{
@@ -131,23 +164,23 @@ if($Value){
 							</select>
 							<p><strong>Commentaire :</strong></p>
 							<textarea name="Commentaire" size="140" rows="4" cols="50" placeholder="Votre commentaire doit être sans accent ni apostrophes !"><?php
-								 if ($Value) {
-										echo "Ligne générée à partir d'une alerte. Vérifiez tout attentivement les informations, elles peuvent ne pas être exactes.";
-								}
-								?>
-							</textarea>
+							if ($Value) {
+								echo "Ligne générée à partir d'une alerte. Vérifiez tout attentivement les informations, elles peuvent ne pas être exactes.";
+							}
+							?>
+						</textarea>
 
-						</td>
-					</table>
-				</fieldset>
-				<input type="submit" name="Enregistrer !">
-			</form>
-
-
+					</td>
+				</table>
+			</fieldset>
+			<input type="submit" name="Enregistrer !">
+		</form>
 
 
 
-		</section>
-		<?php include("include/Footer.php");?>
-	</body>
-	</html>
+
+
+	</section>
+	<?php include("include/Footer.php");?>
+</body>
+</html>
